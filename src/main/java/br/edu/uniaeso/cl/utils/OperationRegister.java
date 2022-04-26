00d607b55f7e17dfeaa7676628c3621ef41f13db
FABIO CESAR FIGUEIREDO DE MEDEIROS CHICOUT <fabio.chicout@ufpe.br>
2022-04-25 21:48:31 -0300

    fixes

--- src/main/java/br/edu/uniaeso/cl/utils/OperationRegister.java
+++ src/main/java/br/edu/uniaeso/cl/utils/OperationRegister.java
@@ -8,23 +8,28 @@ import java.io.IOException;
 public class OperationRegister {
     private OperationRegister instance;
     File RegistroAlunos;
-    private OperationRegister(){}
-    public OperationRegister getInstance(){
-        if(this.instance == null){
+
+    private OperationRegister() {
+    }
+
+    public OperationRegister getInstance() {
+        if (this.instance == null) {
             this.instance = new OperationRegister();
-        }else{
+        } else {
             return this.instance;
         }
         return this.instance;
     }
+
     public void operationLog() throws IOException {
         FileInputStream fis = null;
-        try{
+        try {
             fis = new FileInputStream(RegistroAlunos);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } finally {
-            if (fis != null) fis.close();
+            if (fis != null)
+                fis.close();
         }
     }
 }
